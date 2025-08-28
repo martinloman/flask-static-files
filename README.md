5cd153bf859e68706a4f8a27c6588f77a86e4183
Martin Loman <martin.loman@gmail.com>
2025-08-28 15:23:37 +0200

    Update README and templates to enhance static file usage explanation and improve styling

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Flask Hello World
+# Flask static files demo
 
 A minimal Flask project that demos the use of static files. This demo contains an
 image, a css file and a javascript file.
 
 Static files are stored in the `/static` folder. That folder name is standard convention.
+
+Note the use of the `url_for` function (in the templates) to serve static files. This enables Flask to dynamically generate the correct URLs for static assets, ensuring that they are properly linked in your HTML. It does not matter what route a template is rendered from; the `url_for` function will always point to the correct static file.
 
 ## How to run
 
--- app.py
+++ app.py
@@ -3,14 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 """
-All static files are stored in the 'static' directory. 
-This is the standard location for serving static assets in Flask applications.
-Static content could include images, audio, video, stylesheets, and JavaScript files.
+Alla statiska filer lagras i katalogen 'static'. 
+Detta är den standardplats där Flask-applikationer serverar statiska resurser. 
+Statiskt innehåll kan inkludera bilder, ljud, video, stilmallar och JavaScript-filer.
 """
 
 @app.route('/')
 def index():
     return render_template('index.html', heading="Welcome to the Flask Static Files Demo")
+
+"""
+Trots att den här routen är jättelång så fungerar referenserna till de statiska
+filerna. 
+Det fungerar eftersom mallen använder {{ url_for('static', filename='images/sneakers.png') }}
+istället för den absoluta sökvägen "static/images/sneakers.png".
+"""
+@app.route('/a/really/long/route')
+def index_again():
+    return render_template('index.html', heading="This page is on a long route")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
